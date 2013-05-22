39276febdd7c35e245ded7f48a4b4884d64c4721
Waldek <waldek@ubuntu.(none)>
2013-05-22 13:04:32 +0200

    Fix radio inputs

--- js/jquery.cal.js
+++ js/jquery.cal.js
@@ -1,38 +1,63 @@
-(function($){
-	$.fn.cal = function(options) {
-		
-		var that = this;
-		var settings = $.extend({
-			'link_class'       : 'cal-link',
-			'link_class_marked': 'cal-link-marked'
-		}, options);
-		
-		var draw = function() {
-			that.each(function(){
-				prepareATag($(this));
-			});
-		};
-		
-		var prepareATag = function(input) {
-			var anchor = $('<a href="#"></a>');
-			$(anchor).data('id', input.attr('id')).addClass(settings.link_class);
-			var anchorText = input.parents('label').text();
-			anchor.html(anchorText);
-			
-			anchor.on('click', function(e){
-				e.preventDefault();
-				if(input.is(':checked')) {
-					input.removeAttr('checked');
-					$(anchor).removeClass(settings.link_class_marked ).addClass(settings.link_class);
-				} else {
-					input.attr('checked', 'checked');
-					$(anchor).addClass(settings.link_class_marked ).removeClass(settings.link_class);
-				}
-			});
-			$(anchor).insertAfter(input.parents('label'));
-			input.parents('label').css('position', 'absolute').css('left', '-10000px');
-		}
-		
-		return draw();
-	}
-})(jQuery)
+;(function($) {
+    $.fn.cal = function(options) {
+
+        var that = this;
+        var settings = $.extend({
+            'link_class': 'cal-link',
+            'link_class_marked': 'cal-link-marked'
+        }, options);
+
+        var draw = function() {
+            that.each(function() {
+                prepareATag($(this));
+            });
+        };
+        
+        var prepareCheckbox = function(anchor, input) {
+            anchor.on('click', function(e) {
+                e.preventDefault();
+                if (input.is(':checked')) {
+                    input.removeAttr('checked');
+                    $(anchor).removeClass(settings.link_class_marked).addClass(settings.link_class);
+                } else {
+                    input.attr('checked', 'checked');
+                    $(anchor).addClass(settings.link_class_marked).removeClass(settings.link_class);
+                }
+            });
+        };
+        
+        var prepareRadio = function(anchor, input) {
+            anchor.on('click', function(e) {
+                e.preventDefault();
+                that.each(function(){
+                    $(this).removeAttr('checked');
+                });
+                $('.' + settings.link_class_marked).removeClass(settings.link_class_marked);
+                $(anchor).addClass(settings.link_class_marked);
+                input.attr('checked', 'checked');
+            });
+        };
+
+        var prepareATag = function(input) {
+            var anchor = $('<a href="#"></a>');
+            $(anchor).data('id', input.attr('id')).addClass(settings.link_class);
+            var anchorText = input.parents('label').text();
+            anchor.html(anchorText);
+            
+            switch(input.attr('type')) {
+                case 'checkbox':
+                    prepareCheckbox(anchor, input);
+                    break;
+                case 'radio':
+                    
+                    prepareRadio(anchor, input);
+                    break;
+            }
+            
+            $(anchor).insertAfter(input.parents('label'));
+            input.parents('label').css('position', 'absolute').css('left', '-10000px');
+        };
+
+        return draw();
+    }
+})(jQuery);
